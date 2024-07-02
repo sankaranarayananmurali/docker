ca2a1fbe85a4dd650f7e45a66731565149b8e555
Dayananda Gowda I J <dayanandagowda2002@gmail.com>
2024-07-02 15:37:48 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,6 +1,6 @@
-public class HelloWorld {
+public class Demo {
     public static void main(String[] args) {
         // Print a message to the console
-        System.out.println("Hello, world!");
+        System.out.println("Hello from demo.java!");
     }
 }
