62b511ddcb96a0d4fbc659e0d87a367105be5dff
Sannidhi10 <103745922+Sannidhi10@users.noreply.github.com>
2024-07-23 14:20:11 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,6 +1,6 @@
 public class demo {
     public static void main(String[] args) {
         // Print a message to the console
-        System.out.println("Hello from demo.java! hi I am sreejithe....!");
+        System.out.println("Hello welcome to devops");
     }
 }
