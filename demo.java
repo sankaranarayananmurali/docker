fff8c139ebe0c9e63b80fc267b426dfe152a21f0
Sannidhi10 <103745922+Sannidhi10@users.noreply.github.com>
2024-08-26 19:56:26 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,6 +1,6 @@
 public class demo {
     public static void main(String[] args) {
         // Print a message to the console as output
-        System.out.println("Hello welcome to devops");
+        System.out.println("Hello welcome to jenkins");
     }
 }
