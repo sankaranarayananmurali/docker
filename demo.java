ed97add7c3c9a12c79f3bb5b514e08cd49be6bec
sankaranarayananmurali <88310495+sankaranarayananmurali@users.noreply.github.com>
2025-02-09 11:04:41 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -2,5 +2,6 @@ public class demo {
     public static void main(String[] args) {
         // Print a message to the console as output
         System.out.println("Hello welcome to jenkins");
+        System.out.println("Thanks for welcoming me jenkins");
     } 
 }
